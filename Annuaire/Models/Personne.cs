ffa37b43f9f8ea2cdeda5411c591ba959d8a5b26
RenoufTheophile <theophile.renouf@viacesi.fr>
2021-08-25 13:53:24 +0200

    sauvegarde

---
+++ Annuaire/Models/Personne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Annuaire.Models
+{
+    public class Personne
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Telephone { get; set; }
+        public string Service { get; set; }
+        public DateTime Date_entre { get; set; }
+
+
+    }
+}
