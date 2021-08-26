4592de8aca38bc69ec722c2bfe2382a98691fa4f
RenoufTheophile <theophile.renouf@viacesi.fr>
2021-08-26 14:21:17 +0200

    Sauvegarde 9

---
+++ Annuaire/Controllers/ManagerPerson.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace Annuaire.Controllers
+{
+    public class ManagerPerson : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Attributs
+        private MySqlConnection connexion; 
+        private static string connectionString = "SERVER=127.0.0.1; DATABASE=annuaire ; UID=root; PASSWORD="; 
+        #endregion
+    }
+}
