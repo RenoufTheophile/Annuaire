8400b12821fe3488cf8ea3a8850d8e37662b810a
RenoufTheophile <theophile.renouf@viacesi.fr>
2021-08-27 15:31:19 +0200

    Sauvegarde 11

---
+++ Annuaire/Models/Ramdom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Annuaire.Models
+{
+    public class Ramdom
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Telephone { get; set; }
+        public string Service { get; set; }
+
+        [Display(Name = "Date d'entrer")]
+        [DataType(DataType.Date)]
+        public DateTime Date_entrer { get; set; }
+
+    }
+}
